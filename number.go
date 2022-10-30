c50b419cf6429bd220bf6454c5efafa5aea03c2c
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-10-30 13:18:06 +0700

    Belajar Pengenalan Golang Menampilkan Tipe Data Number

---
+++ number.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// func main() { // sementara di disable bentrok main
+func main2() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga Koma Lima = ", 3.5)
+}
