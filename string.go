eb0ef37aa1053b9f62a3ac6d7c4766d2945f2cd8
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-10-30 13:20:01 +0700

    Belajar Pengenalan Golang Menampilkan Tipe Data String, Menghitung Length String dan Mengambil data berdasarkan index dan menampilkan dalam bentuk bit memory

---
+++ string.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// func main() { // sementara di disable bentrok main
+func main4() {
+
+	fmt.Println("Materi String")
+	fmt.Println("Daus")
+	fmt.Println("Bachtiar Firdaus")
+	fmt.Println("Hello Bachtiar Firdaus")
+
+	fmt.Println("Materi length and index")
+	fmt.Println(len("Daus"))
+	fmt.Println("Bachtiar Firdaus"[0]) // hasil index bentuknya nilai byte dari index
+	fmt.Println("Hello Bachtiar Firdaus"[1])
+}
