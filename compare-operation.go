0afe1ced6f078d3d05e5d7cf9354fa60f8e5b4da
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-10-30 15:02:29 +0700

    Belajar Pengenalan Golang Menggunakan Compare Operation

---
+++ compare-operation.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// func main() { // sementara di disable bentrok main
+func main() {
+	// compare operation adalah operasi perbandingan terhadap dua buah data
+	// hasilnya dalam bentuk false / true (boolean)
+	// pada dasarnya sama dengan bahasa pemrograman lain
+	// example > < >= <= == !=
+	var name1 = "Daus"
+	var name2 = "Daus"
+	var name3 = "Bachtiar"
+	// example compare
+	var result1 = name1 == name2
+	var result2 = name1 == name3
+	fmt.Println("Perbandingan yang sama : ", result1)
+	fmt.Println("Perbandingan yang tidak sama : ", result2)
+
+	// example pada angka
+	var (
+		n1 = 1
+		n2 = 2
+	)
+	fmt.Println(n1 > n2)
+	fmt.Println(n1 < n2)
+	fmt.Println(n1 <= n2)
+	fmt.Println(n1 >= n2)
+	fmt.Println(n1 == n2)
+	fmt.Println(n1 != n2)
+}
