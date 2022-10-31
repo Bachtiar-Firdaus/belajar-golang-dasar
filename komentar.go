be1284a7c13e190ac8eea0dd0eff185e737f4e39
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-11-01 01:07:48 +0700

    Belajar Pengenalan Golang Menggunakan komentar

---
+++ komentar.go
@@ -0,0 +1,12 @@
+package main
+
+func main31() {
+	// komentar
+	// komentar terbaik pada kode adalah kode itu sendiri
+	// saat membuat code kita peru membuat code semudah mungkin untuk dibaca
+	// namun kadang juga kita butuh menambahkan komentar di kode kita
+
+	// example 1
+	/* ini komentar multi line */
+	// ini komentar singgle line
+}
