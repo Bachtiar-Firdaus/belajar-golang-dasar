7c1b9873ed9e3cf8971f3ca1ff6fb2a2fd0270a1
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-11-02 23:46:38 +0700

    Belajar Pengenalan Golang Menggunakan GOPATH

---
+++ gopath.go
@@ -0,0 +1,11 @@
+package main
+
+func main42() {
+	// Aktifkan GOPATH
+	/*
+		gi versi Go-Lang terbaru sekarang sudah tidak
+		direkomendasikan lagi menggunakan GOPATH, melainkan lebih di rekomendasikan menggunakan Go Modules
+		karna Go modules akan dibahas di kelas selanjutnya dan sekarang kita akan menggunakan GOPATH jadi silahkan perintah berikut di terminal untuk mengaktifkan GOPATH
+		go env -w GO111MODULE=off
+	*/
+}
